8a22d2d96ad370660d8028ea5acd226f5f3a2e7f
Andreas <andreas.brandenborg@live.dk>
2025-12-02 11:49:38 +0100

    Changed const in db init.js

--- CarCatalogService/Database/init/init.js
+++ CarCatalogService/Database/init/init.js
@@ -1,10 +1,10 @@
-// Switch to your database (will be created automatically)
+// Switch to your database
 db = db.getSiblingDB("car-catalog-db");
 
 // Create a collection
 db.createCollection("cars");
 
-// Insert sample documents (optional)
+// Insert sample documents
 db.cars.insertMany([
   { make: "Toyota", model: "Corolla", year: 2020 },
   { make: "Honda", model: "Civic", year: 2021 }
