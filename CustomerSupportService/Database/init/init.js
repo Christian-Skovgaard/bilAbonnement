b0953c0ef1e2c78c4e28f9904412ecb61957eda4
magnus <magnus.giemsa@gmail.com>
2025-12-03 12:57:42 +0100

    tilføjet POST og GET med query

--- CustomerSupportService/Database/init/init.js
+++ CustomerSupportService/Database/init/init.js
@@ -6,6 +6,6 @@ db.createCollection("complaints");
 
 // Insert sample documents
 db.complaints.insertMany([
-  { complaintId: "1", name: "magnus", year: 2020 },
-  { complaintId: "2", name: "victor", year: 2021 }
+  { complaintId: 1, name: "magnus", year: 2020 },
+  { complaintId: 2, name: "victor", year: 2021 }
 ]);
--- CustomerSupportService/Service/app.py
+++ CustomerSupportService/Service/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from mongoengine import connect, Document, StringField, IntField
 
-# adjust DB name if needed — your init script used "car-catalog-db"
-# Use the compose service name for the DB host (car-catalog-db)
+
 connect(db='customer-service-db', host='customer-service-db', port=27017)
 
 app = Flask(__name__)
@@ -24,6 +23,38 @@ def get_complaints():
     return jsonify([complaints_to_dict(c) for c in complaints])
 
 
+@app.route('/complaints', methods=['POST'])
+def add_complaints():
+    data = request.get_json(force=True)
+    complaintId = data.get('complaintId')
+    name = data.get('name')
+    year = data.get('year')
+    complaints = Complaint(complaintId=complaintId, name=name, year=year)
+    complaints.save()
+    return jsonify(complaints_to_dict(complaints)), 201
+
+
+#get complaints by query parameters
+@app.route('/complaints/query', methods=['GET'])
+def search_complaints():
+    complaintId = request.args.get('complaintId')
+    name = request.args.get('name')
+    year = request.args.get('year')
+
+    filters = {}
+    
+    if complaintId:
+        filters['complaintId'] = int(complaintId)  
+    
+    if name:
+        filters['name__icontains'] = name
+
+    if year:
+        filters['year'] = int(year)
+
+    complaints = Complaint.objects(**filters)
+
+    return jsonify([complaints_to_dict(c) for c in complaints])
 
 
 if __name__ == '__main__':
