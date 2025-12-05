3a7bd0754505a2da4e7fb88fcd78f620d86d34c8
Andreas <andreas.brandenborg@live.dk>
2025-12-05 10:15:33 +0100

    feat: implement GET endpoint to retrieve all tasks from the database

--- TaskManagementService/Service/app.py
+++ TaskManagementService/Service/app.py
@@ -10,3 +10,12 @@ tasks_collection = db["tasks"]
 
 
 # Get all tasks
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    cursor = tasks_collection.find({}, {"_id": 0})  # Query and remove MongoDB _id (suppress_id)
+    tasks = list(cursor)
+    return jsonify(tasks)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5010)
