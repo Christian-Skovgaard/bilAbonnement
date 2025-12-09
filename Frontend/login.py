762d9e851f42aed0929a7864fde58bce1a797fbf
AndreasRG <andreas@revesz-gudmann.dk>
2025-12-09 11:51:20 +0100

    Feat - Frontend for filter with price and km driven updated. Also auth by guest bug = fixed

--- Frontend/login.py
+++ Frontend/login.py
@@ -3,9 +3,14 @@ from streamlit_cookies_controller import CookieController
 import requests
 
 controller = CookieController()
-if "Authorization" in controller.getAll(): # Hvis du allerede er logget ind.
+
+# Only redirect if both a token and a real username exist
+if "Authorization" in controller.getAll() and st.session_state.get("username") not in (None, "Guest"):
     st.switch_page("pages/cars.py")
-st.query_params = {} # Hvis du er sendt tilbage til login skal alle query params fjernes.
+
+# If you’re back at login, clear query params
+st.query_params = {}
+
 
 st.set_page_config(page_title="Log ind | Bilabonnement", page_icon="⏱️", layout="centered")
 
